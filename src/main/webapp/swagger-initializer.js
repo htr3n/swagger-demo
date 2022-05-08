68aebc5c501fd66730b23a6c304b1791a75d83fc
Alex <alex.tran@infomedix.com.au>
2022-05-08 20:23:42 +1000

    A Swagger 2 demo project  * Configure Swagger core to scan the RESt resources and generate the OpenAPI JSON  * The Maven plugin will download and unpack the specified version of Swagger UI 4.x  * Bundle Swagger UI with a custom swagger-initializer.js to adjust the base URL

--- src/main/webapp/swagger-initializer.js
+++ src/main/webapp/swagger-initializer.js
@@ -1,5 +1,10 @@
-// See: https://stackoverflow.com/a/71210264/339302
-const UrlMutatorPlugin = (system) => ({
+/**
+ * This is a Swagger plugin that updates the base URL to the provided input URL.
+ * As Swagger UI does not adjust the base URL correctly, this is a workaround based on the
+ * suggestion in https://stackoverflow.com/a/71210264/339302.
+ * Also see: https://swagger.io/docs/open-source-tools/swagger-ui/customization/plugin-api/
+ */
+const BaseUrlSettingPlugin = (system) => ({
   rootInjects: {
     setServer: (server) => {
       const jsonSpec = system.getState().toJSON().spec.json;
@@ -12,8 +17,9 @@ const UrlMutatorPlugin = (system) => ({
 
 window.onload = function () {
   const base_url = window.location.origin + window.location.pathname;
+  const base_url_no_ending_slash = base_url.replace(/\/$/, '');
   window.ui = SwaggerUIBundle({
-    url: `${base_url}api/openapi.json`,
+    url: `${base_url_no_ending_slash}/api/openapi.json`,
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
@@ -22,11 +28,10 @@ window.onload = function () {
     ],
     plugins: [
       SwaggerUIBundle.plugins.DownloadUrl,
-      UrlMutatorPlugin
+      BaseUrlSettingPlugin
     ],
     layout: "StandaloneLayout",
     onComplete: () => {
-      const base_url_no_ending_slash = base_url.replace(/\/$/, '');
       window.ui.setServer(base_url_no_ending_slash);
     }
   });
